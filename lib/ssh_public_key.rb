0f12ee428672dce74ecbc3a1c7289c8c78757a04
Jeremy Voorhis <jeremy@phpfog.com>
2011-03-28 13:32:35 -0700

    Extracted two's complement helper.

--- lib/ssh_public_key.rb
+++ lib/ssh_public_key.rb
@@ -1,19 +1,4 @@
-class Integer
-  def self.from_twos_complement(bytestring)
-    bytestring.unpack("cC*").inject(0) { |i,b| (i << 8) | b }
-  end
-
-  def to_twos_complement
-    int = self
-    bytearray = []
-    begin
-      bytearray << (int & 0xff)
-      int >>= 8
-    end until (int == 0 || int == -1) && (bytearray.last[7] == int[7])
-    bytearray.reverse!
-    bytearray.pack("c" + "C*")
-  end
-end
+require 'twos_complement'
 
 module SSHPublicKey
 
