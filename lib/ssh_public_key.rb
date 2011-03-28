f44ae5b39d1961c1c3d2c1417052ff373681894d
Jeremy Voorhis <jeremy@phpfog.com>
2011-03-28 13:31:35 -0700

    Reading and writing OpenSSH-style public keys is now supported
    * Added BlobWriter to encode raw data
    * Cleaned up BlobReader
    * Eliminated PublicKey because its subclasses had little in common
    * Factored out two's complement helpers for Integer

--- lib/ssh_public_key.rb
+++ lib/ssh_public_key.rb
@@ -1,84 +1,156 @@
-require 'base64'
+class Integer
+  def self.from_twos_complement(bytestring)
+    bytestring.unpack("cC*").inject(0) { |i,b| (i << 8) | b }
+  end
+
+  def to_twos_complement
+    int = self
+    bytearray = []
+    begin
+      bytearray << (int & 0xff)
+      int >>= 8
+    end until (int == 0 || int == -1) && (bytearray.last[7] == int[7])
+    bytearray.reverse!
+    bytearray.pack("c" + "C*")
+  end
+end
 
 module SSHPublicKey
+
   module_function
 
   def parse(text)
     plaintext_sigil, blob, comment = text.split(/\s/)
-    bytes = Base64.decode64(blob)
+    bytes = blob.unpack("m").first
     case text
     when /^ssh-rsa/
-      sigil, e, m = BlobParser.parse(bytes, :string, :bigint, :bigint)
-      RSAPublicKey.new(:parameters => { 'e' => e, 'm' => m }, :comment => comment)
+      sigil, e, m = BlobReader.new(bytes).read_string.
+                                          read_bigint.
+                                          read_bigint.values
+      RSAPublicKey.new(:e => e, :m => m, :comment => comment)
     when /^ssh-dss/
-      sigil, p, q, g, y = BlobParser.parse(bytes, :string, :bigint, :bigint, :bigint, :bigint)
-      DSAPublicKey.new(:parameters => { 'p' => p, 'q' => q, 'g' => g, 'y' => y }, :comment => comment)
+      sigil, p, q, g, y = BlobReader.new(bytes).read_string.
+                                                read_bigint.
+                                                read_bigint.
+                                                read_bigint.
+                                                read_bigint.values
+      DSAPublicKey.new(:p => p, :q => q, :g => g, :y => y, :comment => comment)
     end
   end
 
-  class BlobParser < Struct.new(:bytes, :pos, :value)
-    def self.parse(bytes, *fields)
-      values = []
-      initial_parser = BlobParser[bytes, 0, nil]
-      values, final_parser = fields.inject([values, initial_parser]) do |(values, parser), field|
-        parser.send(:"read_#{field}") do |parser|
-          [values + Array(parser.value), parser]
-        end
-      end
-      values
-    end
+  class BlobReader
+    attr_reader :bytes, :pos, :values
 
-    def read(type)
-      send(:"read_#{type}") { |parser| yield parser }
+    def initialize(bytes, pos = 0, values = [])
+      @bytes  = bytes
+      @pos    = pos
+      @values = values
     end
 
     def read_string
-      read_int do |int|
-        size  = int.value
-        value = bytes[int.pos, size].unpack("a*").first
-        yield BlobParser[bytes, int.pos+size, value]
-      end
+      int = read_int
+      size = int.values.last
+      value = bytes[int.pos, size].unpack("a*").first
+      BlobReader.new(bytes, int.pos + size, values + [value])
     end
 
     def read_int
       size  = 4
       value = bytes[pos, size].unpack("N").first
-      yield BlobParser[bytes, pos + size, value]
+      BlobReader.new(bytes, pos + size, values + [value])
     end
 
     def read_bigint
-      read_int do |int|
-        size  = int.value
-        value = bytes[int.pos, size].unpack("c" + "C" * (size-1)).inject(0) { |i,b| (i << 8) | b }
-        yield BlobParser[bytes, int.pos + size, value]
-      end
+      int   = read_int
+      size  = int.values.last
+      value = Integer.from_twos_complement(bytes[int.pos, size])
+      BlobReader.new(bytes, int.pos + size, values + [value])
     end
   end
 
-  class PublicKey
-    private_class_method :new
+  class BlobWriter
+    attr_reader :bytes
 
-    attr_reader :parameters, :comment
+    def initialize(bytes = "")
+      @bytes = bytes
+    end
 
-    def initialize(options = {})
-      @parameters = options[:parameters]
-      @comment    = options[:comment]
+    alias :to_s :bytes
+
+    def write_int(int)
+      BlobWriter.new(bytes + [int].pack("N"))
+    end
+
+    def write_string(string)
+      bytestring = [string].pack("a*")
+      BlobWriter.new(
+        write_int(bytestring.bytesize).bytes + bytestring)
+    end
+
+    def write_bigint(bigint)
+      bytestring = bigint.to_twos_complement
+      BlobWriter.new(
+        write_int(bytestring.bytesize).bytes + bytestring)
     end
   end
 
-  class RSAPublicKey < PublicKey
-    public_class_method :new
+  class RSAPublicKey
+    attr_reader :e, :m, :comment
+
+    def initialize(params)
+      @e       = params[:e]
+      @m       = params[:m]
+      @comment = params[:comment]
+    end
 
     def algorithm
       'RSA'
     end
+
+    def sigil
+      'ssh-rsa'
+    end
+
+    def blob
+      bytes = BlobWriter.new.write_string(sigil).write_bigint(e).write_bigint(m).to_s
+      [bytes].pack("m").tr("\n", "")
+    end
+
+    def to_s
+      [sigil, blob, comment].join(" ")
+    end
   end
 
-  class DSAPublicKey < PublicKey
-    public_class_method :new
+  class DSAPublicKey
+    attr_reader :p, :q, :g, :y, :comment
+
+    def initialize(params)
+      @p       = params[:p]
+      @q       = params[:q]
+      @g       = params[:g]
+      @y       = params[:y]
+      @comment = params[:comment]
+    end
 
     def algorithm
       'DSA'
+    end
+
+    def sigil
+      'ssh-dss'
+    end
+
+    def blob
+      bytes = BlobWriter.new.write_string(sigil).
+                             write_bigint(p).
+                             write_bigint(q).
+                             write_bigint(g).
+                             write_bigint(y).to_s
+      [bytes].pack("m").tr("\n", "")
+    end
+
+    def to_s
+      [sigil, blob, comment].join(" ")
     end
   end
 end
